a84d9163153780532671e64021f8641c31e7a5ba
segawadda <sagarromer@gmail.com>
2021-06-12 13:33:40 +0530

    video controls cretaed

--- script.js
+++ script.js
@@ -9,6 +9,37 @@ menu.addEventListener('click', () =>{
 });
 //end of navbar
 
+//section 2 video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+const playPause=() =>{
+    if(video.paused){
+        video.play();
+        btn.className = 'far fa-pause-circle';
+        video.style.opacity ='.7';
+
+    }else{
+        video.pause();
+        btn.className = 'far fa-play-circle';
+        video.style.opacity ='.3';
+    }
+};
+btn.addEventListener('click',()=>{
+    playPause();
+})
+video.addEventListener('timeupdate', () =>{
+    const barWidth = video.currentTime/video.duration;
+    bar.style.width = `${barWidth * 100}%`;
+    if(video.ended){
+        btn.className = 'far fa-play-circle';
+        video.style.opacity = '0.3';
+    }
+})
+
+
+
 
 
 
